b89710d19274ada580aa696e1dca0dce4d6cd7a4
sumit patel <sp43216@gmail.com>
2024-02-11 23:39:15 +0530

    a

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ if(working == false){
 const login = async() => {
 const url = "ws://localhost:9222/devtools/browser/a5e90073-bd04-4f73-ab37-208aa46a93c4"
 const browser = await pupeteer.launch({headless:'new',
-  browserWSEndpoint:url,
+  // browserWSEndpoint:url,
    args: ["--no-sandbox"]
   // executablePath:"C:/Program Files (x86)/AVAST Software/Browser/Application/AvastBrowser.exe"
   // executablePath:"C:/Program Files/Google/Chrome/Application/chrome.exe"
